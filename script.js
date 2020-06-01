5bef09b5923dec93f1871c443b9115efdd294c91
JuiciiYT <56958664+JuiciiYT@users.noreply.github.com>
2020-06-01 10:20:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-alert("Starting Secret Webpage");
-confirm("psst. Scroll Down!");
+$(document).ready(function() {
+		$('#material-tabs').each(function() {
+
+				var $active, $content, $links = $(this).find('a');
+
+				$active = $($links[0]);
+				$active.addClass('active');
+
+				$content = $($active[0].hash);
+
+				$links.not($active).each(function() {
+						$(this.hash).hide();
+				});
+
+				$(this).on('click', 'a', function(e) {
+
+						$active.removeClass('active');
+						$content.hide();
+
+						$active = $(this);
+						$content = $(this.hash);
+
+						$active.addClass('active');
+						$content.show();
+
+						e.preventDefault();
+				});
+		});
+});
