ea694d0dab7b6f8549e9a011b21cdff2d70347c8
JuiciiYT <56958664+JuiciiYT@users.noreply.github.com>
2020-06-28 12:58:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ $(document).ready(function() {
 				});
 		});
 });
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
